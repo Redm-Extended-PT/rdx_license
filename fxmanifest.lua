88c03f218ac4e1378144a4abdb96143dfcf7ff4f
Nevermind <58900773+RalivTV@users.noreply.github.com>
2021-10-24 03:30:06 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+game 'rdr3'
+fx_version 'adamant'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+description 'RDX License'
+
+version '1.0.1'
+
+server_scripts {
+	'@async/async.lua',
+	'@mysql-async/lib/MySQL.lua',
+	'server/main.lua'
+}
